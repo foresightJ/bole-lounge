f1d53138abde13d25b8e93a463309ca7ac422a39
John U. Amah <84844009+foresightJ@users.noreply.github.com>
2022-04-09 12:26:28 -0400

    added userlogout component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import MenuList from './components/MenuList/MenuList';
+import About from './components/About/About';
+import Category from './components/Category/Category';
+import Contact from './components/Contact/Contact';
+import FoodDetail from './components/FoodDetail/FoodDetail';
+import HomePage from './components/HomePage/HomePage';
+import MyProfile from './components/MyProfile/MyProfile';
+import OrderDetail from './components/OrderDetail/OrderDetail';
+import OrderHistory from './components/OrderHistory/OrderHistory';
+import Payment from './components/Payment/Payment';
+import ShoppingCart from './components/ShoppingCart/ShoppingCart';
+import UserLogOut from './components/UserLogOut/UserLogOut';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className='App'>
+				<header className='App-header'>Bole Lounge</header>
+				<MenuList />
+				<About />
+				<Category />
+				<Contact />
+				<FoodDetail />
+				<HomePage />
+				<MyProfile />
+				<OrderDetail />
+				<OrderHistory />
+				<Payment />
+				<ShoppingCart />
+				<UserLogOut />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/UserLogOut/UserLogOut.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function UserLogOut(props) {
+	return <div className='component'>UserLogOut</div>;
+}
